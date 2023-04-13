380cacdeb422bc72a74cf01445a135ff2d6601fe
dragonwarrior980706 <dragon.warrior980706@gmail.com>
2023-04-13 04:35:48 +0300

    updated

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,16 @@
-$('#mobile-btn-1').on('click', function (e) {
-  $('#address-items').toggleClass('d-none');
-});
+// $('#mobile-btn-1').on('click', function (e) {
+//   $('#address-items').toggleClass('d-none');
+// });
 
-$('#mobile-btn-2').on('click', function(e) {
-  $('#menu-items').toggleClass('d-none');
-});
+// $('#mobile-btn-2').on('click', function(e) {
+//   $('#menu-items').toggleClass('d-none');
+// });
+
+$('#mobile-btn-2').click(() => {
+  // $('#menu-items').removeClass('d-none');
+  $('#menu-items').slideToggle("2000")
+})
+$('#mobile-btn-1').click(() => {
+  // $('#address-items').removeClass('d-none');
+  $('#address-items').slideToggle("2000")
+})
